06db9fd6facc9e973af1c4be46e8513e327d98d9
Sander in 't Veld <sanderintveld@hotmail.com>
2022-01-21 20:47:57 +0100

    Trying to understand it, but it always has maximal loopage. So maybe it really is just a magic machine.

--- src/music.rs
+++ src/music.rs
@@ -30,6 +30,7 @@ pub fn play(tick: usize, seed: usize, ground_freq: u32, broken: bool)
 
 	let t = tick / RATE;
 	let note = get_note_from_t(t, seed, broken);
+	trace(format!("{}", note));
 	play_note(note, ground_freq, broken)
 }
 
@@ -67,48 +68,37 @@ mod tests
 {
 	use super::*;
 
-	const LOOP_LENGTH: usize = 313 * 12;
+	const MAX_LOOP_LENGTH: usize = 313 * 12;
 
 	#[test]
-	fn test_loop_length()
+	fn print_loop_length()
 	{
-		for seed in 7..487
+		for seed in 1usize..313usize
 		{
-			let first_notes =
-				(0..LOOP_LENGTH).map(|t| get_note_from_t(t, seed, false));
-			let second_notes = (0..LOOP_LENGTH)
-				.map(|t| get_note_from_t(t + LOOP_LENGTH, seed, false));
-			for (t, (a, b)) in first_notes.zip(second_notes).enumerate()
+			let mut first_notes = [0i8; MAX_LOOP_LENGTH];
+			for t in 0..MAX_LOOP_LENGTH
 			{
-				assert_eq!(a, b, "note mismatch at {} in seed {}", t, seed);
+				first_notes[t] =
+					((seed.wrapping_mul(t) % 313) % SCALE_SIZE) as i8
+			}
+			for offset in 1..=313
+			{
+				let second_notes = (0..MAX_LOOP_LENGTH).map(|t| {
+					((seed.wrapping_mul(t + offset) % 313) % SCALE_SIZE) as i8
+				});
+				if first_notes.iter().zip(second_notes).all(|(a, b)| *a == b)
+				{
+					if offset < 313
+					{
+						println!("{} loops every {} frames", seed, offset);
+					}
+					else
+					{
+						println!("{} achieves maximal loopage!", seed);
+					}
+					break;
+				}
 			}
 		}
-	}
-
-	#[test]
-	fn print_unique_seed()
-	{
-		let mut uniques: Vec<(usize, [i8; LOOP_LENGTH])> = Vec::new();
-		for seed in 1..487
-		{
-			let mut notes = [0i8; LOOP_LENGTH];
-			for t in 0..LOOP_LENGTH
-			{
-				notes[t] = get_note_from_t(t, seed, false);
-			}
-			let is_matched = uniques.iter().any(|(_, seed_notes)| {
-				seed_notes.iter().zip(notes.iter()).all(|(a, b)| a == b)
-			});
-			if !is_matched
-			{
-				uniques.push((seed, notes));
-			}
-		}
-		println!("There are {} unique seeds:", uniques.len());
-		for (seed, _) in uniques
-		{
-			print!("{} ", seed);
-		}
-		println!();
 	}
 }
